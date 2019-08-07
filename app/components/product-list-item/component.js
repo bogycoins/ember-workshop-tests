96c10c0fe264404b7dffacdcf61b411a07562676
Bogdan Olaru <bogdan.olaru@teamaol.com>
2019-08-08 01:38:37 +0300

    tests

--- app/components/product-list-item/component.js
+++ app/components/product-list-item/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['col-md-4']
+  classNames: ['col-md-4'],
+  i: 0,
+
+  actions:{
+    changeState(){
+      this.toggleProperty('isEnabled');
+      this.set('i', this.get('i') + 1);
+    }
+  }
 });
